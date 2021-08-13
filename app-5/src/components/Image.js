04a2e2ea977f8b5c2947c21d940d8e03d44e84b7
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-13 12:20:31 -0500

    added app-5 solution

---
+++ app-5/src/components/Image.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class Image extends Component{
+
+    render(){
+        // console.log(this.props.url)
+        // const {url} = this.props
+        return(
+            <div>
+                <img src={this.props.url} alt="disguised-kitten"/>
+            </div>
+        )
+    }
+}
